61a8ef48a56f09d69dbd4aeb3e5ab47fb257f087
chanliu3 <chanliu3@cisco.com>
2018-01-24 14:39:33 +0800

    fix bug that log not included

---
+++ python/system/log/__init__.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__all__ = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL',
+          'FORMAT_DATA', 'FORMAT_LEVEL', 'FORMAT_RELATIVE', 'FORMAT_FUNC', 'FORMAT_LOGGER',
+          'getLogger', 'getStreamLogger', 'getFileLogger', 'scopelog']
+
+import logging
+
+DEBUG    = logging.DEBUG
+INFO     = logging.INFO	
+WARNING  = logging.WARNING 
+ERROR    = logging.ERROR 
+CRITICAL = logging.CRITICAL
+
+FORMAT_DATA     = '%(message)s'
+FORMAT_LEVEL    = '%(asctime)s - %(levelname)s - %(message)s'
+FORMAT_RELATIVE = '%(relativeCreated)s - %(levelname)s - %(message)s'	
+FORMAT_FUNC     = '%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d; %(module)s:%(funcName)s()] - %(message)s'
+FORMAT_LOGGER   = '%(asctime)s - [%(name)s] - %(message)s'
+
+def getLogger(module='log', file='', level=None, format=None):
+    if( file == '' ):
+        handler = logging.StreamHandler()
+    else:
+        handler = logging.FileHandler(file)
+
+    if( level != None):
+        handler.setLevel(level)
+    if( format != None):
+        handler.setFormatter(logging.Formatter(format))
+
+    logger = logging.getLogger(module)
+
+    if( level != None):
+        logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
+
+def getStreamLogger(module='log', level=DEBUG, format=FORMAT_FUNC):
+    return getLogger(module, '', level, format)
+    
+def getFileLogger(module='log', file='log/log.log', level=INFO, format=FORMAT_LOGGER):
+    return getLogger(module, file, level, format)
+
+class scopelog:
+    'scopelog: log with enter and exit scope block'
+    __depth = 0
+	
+    def __init__(self, module='scopelog', file='', level=DEBUG, format=FORMAT_LOGGER):
+        self.logger = getLogger(module, file, level, format)
+        scopelog.__depth +=1
+        self.__log('Enter (%d)' % scopelog.__depth)
+
+    def __del__(self):
+        self.__log('Exit (%d)' % scopelog.__depth)
+        scopelog.__depth -=1		
+		
+    def __log(self, content):
+	    self.logger.debug(content)
+		
+    def depth():
+        return scopelog.__depth
+
+class crash(ValueError):
+    pass
--- python/watch.py
+++ python/watch.py
@@ -4,10 +4,10 @@
 from finance import *
 
 
-
 watchProfile={
-    "symbols"            : ["ETH/BTC"],
-    "exchanges"          : ["huobipro", "bitfinex"],
+#    "symbols"            : ["ETH/BTC"],
+    "symbols"            : ['BTC/USDT','ETH/BTC','ETH/USDT','LTC/BTC','LTC/USDT', 'BCH/USDT','ETC/USDT','BTC/EUR','BTC/USD','ETH/USD','LTC/USD','BCH/BTC','BCH/ETH','BCH/USD','ETC/BTC','ETC/USD'],
+    "exchanges"          : ["huobipro", "bitfinex", "binance","okex"],
     "loop_period"        : 30,
     "rate_limit"         : 2,
     "order_expire"       : 2,
