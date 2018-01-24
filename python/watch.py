145a8e333f48b32fb98af27cceb7da7a9b5a26d1
chanliu3 <chanliu3@cisco.com>
2018-01-24 15:44:04 +0800

    adjust exchanges

--- python/watch.py
+++ python/watch.py
@@ -6,8 +6,9 @@ from finance import *
 
 watchProfile={
     "symbols"            : ['BTC/USDT','ETH/BTC','ETH/USDT','LTC/BTC','LTC/USDT', 'BCH/USDT','ETC/USDT','BTC/EUR','BTC/USD','ETH/USD','LTC/USD','BCH/BTC','BCH/ETH','BCH/USD','ETC/BTC','ETC/USD'],
-#    "exchanges"          : ["gdax", "huobipro", "binance", "bithumb", "okex", "bitfinex", "kucoin", "bittrex", "poloniex", "okcoinusd", "bitstamp"],
-    "exchanges"          : ["gdax", "huobipro", "binance", "bithumb", "okex", "bitfinex", "kucoin", "bittrex", "okcoinusd", "bitstamp"],
+#    "exchanges"          : ["gdax", "huobipro", "binance", "bithumb", "okex", "bitfinex", "kucoin", "bittrex", "poloniex", "okcoinusd", "bitstamp"],  # all top
+#    "exchanges"          : ["gdax", "huobipro", "binance", "bitfinex", "kucoin", "bittrex", "poloniex", "bitstamp"],   # usa available
+    "exchanges"          : ["gdax", "huobipro", "binance", "bithumb", "okex", "bitfinex", "kucoin", "bittrex", "okcoinusd", "bitstamp"], # china available
     "loop_period"        : 10,
     "rate_limit"         : 2,
     "order_expire"       : 2,
